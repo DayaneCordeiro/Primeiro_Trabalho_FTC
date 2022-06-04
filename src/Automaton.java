b55badcb9dc9df28d4a1b302f319043d848d417f
DayaneCordeiro <dayane.cordeirogs@gmail.com>
2022-06-04 16:56:55 -0300

    feature :: uploading files

--- src/Automaton.java
+++ src/Automaton.java
@@ -96,29 +96,93 @@ public class Automaton {
          *      ... só seguir a mesma lógica
          */
 
+        // Pega os estados incial e finais
+        String automatonInitialState = getAutomatonInitialState(this);
+        ArrayList<String> automatonFinalStates = getAutomatonFinalStates(this);
+
         // criação da estrutura que irá conter os estados do automato não deterministico
         ArrayList<String> currentStatesControl = new ArrayList<String>();
+        ArrayList<String> nextStatesControl = new ArrayList<String>();
+        ArrayList<String> labelsControl = new ArrayList<String>();
         ArrayList<Transition> currentTransitionsControl = new ArrayList<Transition>();
+
+        ArrayList<State> finalStates = new ArrayList<State>();
+        ArrayList<Transition> finalTransitions = new ArrayList<Transition>();
+
+        StringBuilder label = null;
+        int newStateIndex = 1;
+
+        boolean isInitialState = false;
+        boolean isFinalState = false;
 
         // Pega o primeiro estado do automato e insere na variável de controle
         currentStatesControl.add(this.states.get(0).id);
 
         while (!currentStatesControl.isEmpty()) {
             // percorrer todos os estados atuais ([A] - tem só um estado [ABC] - tem mais de um estado pra avaliar)
-            for (String currentState : currentStatesControl) {
 
-                // percorrer as transições procurando todas em que o fromState é esse e ver se é 0 ou 1
-                for (Transition transition : this.transitions) {
-                    if (transition.fromState == currentState) {
-                        if (transition.valueRead == "0") {
-                            // faz algo
-                        } else if (transition.valueRead == "1") {
-                            // faz outro algo
+            // faz primeiro para o 0 <- depois é melhor jogar pra uma função
+            for (String currentState : currentStatesControl) {
+                // percorrer as transições procurando todas em que o fromState é esse e ver se é 0
+                for (Transition transition : this.transitions) {                        // percorre as transições do automato lido
+                    if (transition.fromState == currentState) {                         // se o estado de origem do automato for igual ao estado atual
+                        if (transition.valueRead == "0") {                              // se o valor de transição for o 0
+                            nextStatesControl.add(transition.toState);                  // adiciona o estado para onde está indo na variável de controle
+
+                            if (label != null) {                                        // se a label ja tiver conteúdo, concatena no início dela
+                                label.insert(0, transition.toState + ",");
+                            } else {
+                                label = new StringBuilder(transition.toState);          // se a label ainda estiver vazia, só insere o novo dado
+                            }
+
+                            if (automatonInitialState == currentState) {
+                                isInitialState = true;
+                            }
+
+                            if (automatonFinalStates.contains(currentState)) {
+                                isFinalState = true;
+                            }
+
                         }
                     }
                 }
-
             }
+
+            // cria o estado de origem se não existir (vai acontecer apenas na primeira iteração)
+            if (finalStates.isEmpty()) {
+                State state = new State("0",
+                        "q0",
+                        this.states.get(0).isInitial,
+                        this.states.get(0).isFinal
+                );
+
+                state.setLabel(this.states.get(0).id);
+
+                finalStates.add(state);
+            }
+
+            // insere o novo estado quando não existe
+            if (!labelsControl.contains(String.valueOf(label))) {
+                State state = new State(String.valueOf(newStateIndex),
+                        "q" + newStateIndex,
+                        isInitialState,
+                        isFinalState
+                );
+
+                state.setLabel(String.valueOf(label));
+
+                finalStates.add(state);
+
+                labelsControl.add(String.valueOf(label));
+                newStateIndex++;
+            }
+
+
+
+            isInitialState = false;
+            isFinalState = false;
+
+
         }
 
 
@@ -216,4 +280,36 @@ public class Automaton {
 //        FilesManipulation filesManipulation = new FilesManipulation();
 //        filesManipulation.writeAutomatonConvertedFile();
 //    }
+
+    /**
+     *
+     * @param automaton
+     * @return
+     */
+    public String getAutomatonInitialState(Automaton automaton) {
+        for (State state : automaton.states) {
+            if (state.isInitial) {
+                return state.id;
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     *
+     * @param automaton
+     * @return
+     */
+    public ArrayList<String> getAutomatonFinalStates(Automaton automaton) {
+        ArrayList<String> finalStates = new ArrayList<>();
+
+        for (State state : automaton.states) {
+            if (state.isFinal) {
+                finalStates.add(state.id);
+            }
+        }
+
+        return finalStates;
+    }
 }
